020b8941cf2f3e0d547d2ffb4db44f59858ff9a0
Huevos <79326577+CyHuevos@users.noreply.github.com>
2021-02-19 11:28:59 -0600

    Add files via upload

---
+++ aaaaaaaa.py
@@ -0,0 +1,3 @@
+a=float(input("Ingresa un numnero"))
+c=a*.15
+print("El 15%", " de ", a, "es: ", c)
---
+++ aaaaaaaaa.py
@@ -0,0 +1,6 @@
+print("Ingresa los grados farenheight a convertir")
+a=float(input())
+b=(5/9)
+c=a*32
+d=b-c
+print("El resultado es: ", d)
